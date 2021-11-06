7eb186830694e863a92f8b0432a87c9bdaab3b39
mendoza000 <omarmendoza.alv25@gmail.com>
2021-11-06 00:41:11 -0400

    añadido seccion de servicion v1

--- src/PortfolioApp.jsx
+++ src/PortfolioApp.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import AboutMe from './components/AboutMe'
+import Services from './components/Services'
 
 const PortfolioApp = () => {
 	
@@ -8,6 +9,7 @@ const PortfolioApp = () => {
 		<>
 			<Header />
 			<AboutMe />
+			<Services />
 		</>
 	)
 
---
+++ src/components/Services.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Services.css'
+
+const Services = () => {
+	
+	return(
+		<div className="services">
+			<h3 className="services_title">
+				Servicios
+			</h3>
+
+			<div className="services_list">
+				
+				<div className="services_card">
+					<box-icon name='code-alt' size="cssSize" color="#082032"></box-icon>
+					<p className="services_card_title">
+						Desarrollo Web
+					</p>
+					<p className="services_card_text">
+						Puedo desarrollar un sitio web que cubra sus necesidades y se adapte a usted y su negocio.
+					</p>
+				</div>
+
+				<div className="services_card">
+					<box-icon name='mobile-alt' size="cssSize" color="#082032"></box-icon>
+					<p className="services_card_title">
+						Diseño responsive
+					</p>
+					<p className="services_card_text">
+						Las paginas web no se ven siempre desde una pantalla igual, a veces desde un telefono, tablet o laptop. Yo puedo hacer que se vea bien en todos lados.
+					</p>
+				</div>
+
+				<div className="services_card">
+					<box-icon name='palette' size="cssSize" color="#082032"></box-icon>
+					<p className="services_card_title">
+						Adaptibilidad de diseño
+					</p>
+					<p className="services_card_text">
+						Tambien puedo trabajar con algun diseño que usted ya tenga, o bien, crear uno desde cero con algunon patrones o colores que le llamen la atención.
+					</p>
+				</div>
+
+			</div>
+		</div>
+	)
+
+}
+
+export default Services
