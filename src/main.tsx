e1794c2d6059df5a45b817186cdcd3c40ee2c48c
mendoza000 <omarmendoza.alv25@gmail.com>
2023-03-28 01:03:10 -0400

    fix bugs

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PortfolioApp from "./PortfolioApp";
+import "normalize.css";
+import "boxicons";
+import "./index.css";
+
+ReactDOM.render(<PortfolioApp />, document.getElementById("root"));
